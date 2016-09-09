5baf4bfcb6ec07e6a902e20496d93dc9422ded65
Romul <romul.zurov@yandex.ru>
2016-09-09 16:58:53 +0300

    static added

---
+++ pastoj.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/about')
+def about():
+    return 'pastes for all :)'
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(description='Development Server Help')
+    parser.add_argument("-d", "--debug", action="store_true",
+                        dest="debug_mode",
+                        help="run in debug mode (for use with PyCharm)",
+                        default=False)
+    parser.add_argument("-p", "--port", dest="port",
+                        help="port of server (default:%(default)s)",
+                        type=int, default=5000)
+    cmd_args = parser.parse_args()
+    app_options = {"port": cmd_args.port}
+
+    if cmd_args.debug_mode:
+        app_options["debug"] = True
+        app_options["use_debugger"] = False
+        app_options["use_reloader"] = False
+
+    app.run(**app_options)
+
+
+if __name__ == '__main__':
+    main()
