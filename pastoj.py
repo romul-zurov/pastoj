e0505379f00b91dab2d0aa861dc875a816b3f42a
Romul <romul.zurov@yandex.ru>
2016-09-10 20:39:22 +0300

    index.html template added

--- pastoj.py
+++ pastoj.py
@@ -1,17 +1,27 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 from flask import Flask, render_template
-app = Flask(__name__)
+
+
+class YourFlask(Flask):
+    def create_jinja_environment(self):
+        self.config['TEMPLATES_AUTO_RELOAD'] = True
+        return Flask.create_jinja_environment(self)
+app = YourFlask(__name__)
+# app = Flask(__name__)
+
+app.jinja_env.line_statement_prefix = '%'
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
 
 
 @app.route('/about')
 def about():
-    return render_template('base.html', content="pastes for all :)")
+    return render_template('base.html', raw_content="pastes for all :)")
 
 
 def main():
@@ -27,10 +37,15 @@ def main():
     cmd_args = parser.parse_args()
     app_options = {"port": cmd_args.port}
 
+    # if cmd_args.debug_mode:
+    #     app.port = cmd_args.port
+    #     app.debug = True
+    # app.run()
+
+    app_options["debug"] = cmd_args.debug_mode
     if cmd_args.debug_mode:
-        app_options["debug"] = True
         app_options["use_debugger"] = False
-        # app_options["use_reloader"] = False
+        app_options["use_reloader"] = False
 
     app.run(**app_options)
 
